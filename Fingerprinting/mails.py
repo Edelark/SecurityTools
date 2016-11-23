6d286c8b0773f15dc0807e29dd054a96b055c0fd
Antoni Cobos Domínguez <antonigalileo@gmail.com>
2016-11-23 22:04:12 +0100

    version 1.0
    Style formatted with a pyLint puntuation of 8/10

--- Fingerprinting/mails.py
+++ Fingerprinting/mails.py
@@ -1,21 +1,21 @@
-"Made by Antoni Cobos to github.com/Edelark/SecurityTools V1.0 (only source files in txt)"
-
+"""Made by Antoni Cobos to github.com/Edelark/SecurityTools V1.0 (only source
+   files in txt)"""
 import re
-
-def findMails(source,destination):
-    """[EN] This function find emails in a document and put all finded emails into other file. -> (source,destination)
-    [ES] Esta función recibe un archivo donde buscará los emails y un fichero destino donde almacenarlos. -> (origen,destino)"""
+def find_mail(source, destination):
+    """[EN] This function find emails in a document and put all finded emails
+    into other file. -> (source,destination)
+    [ES] Esta función recibe un archivo donde buscará los emails y un fichero
+    destino donde almacenarlos. -> (origen,destino)"""
     try:
-        manf = open(source,'r')
-        file_exit = open(destination,'a+')
+        manf = open(source, 'r')
+        file_exit = open(destination, 'a+')
         for line in manf:
             string_a = line.strip()
             emails = re.findall('\S+@\S+', string_a)
             for email in emails:
                 file_exit.write(email+'\n')
-
         manf.close()
         file_exit.close()
-        print ("DONE")
-    except:
-        print ("The file ",source," could not be opened...")
+        print("DONE")
+    except ValueError:
+        print("The file ", source, " could not be opened...")
